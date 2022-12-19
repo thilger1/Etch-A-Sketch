da92f8b5bcf9878f29f740da89b6086458aff212
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-12-19 18:46:06 -0500

    functionality for buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const blackButton = document.querySelector('blackButton');
+const rainbowButton = document.querySelector('rainbowButton');
+const eraserButton = document.querySelector('eraserButton');
+
+let blackMode = true;
+let rainbowMode = false;
+let eraserMode = false;
+
 function makeGrid(dimensions = 16){
     let grid = document.querySelector('.grid');
     grid.style.gridTemplateColumns = `repeat(${dimensions} , 1fr)`;
@@ -7,7 +15,36 @@ function makeGrid(dimensions = 16){
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');  
         grid.insertAdjacentElement('beforeend', pixel);
+        pixel.addEventListener('mouseover', () => {
+            if (blackMode){
+                pixel.style.backgroundColor = 'black';
+            }
+            if (rainbowMode){
+                pixel.style.backgroundColor = 'red';
+            }
+            if (eraserMode){
+                pixel.style.backgroundColor = 'white';
+            }
+        });
     }
 };
 
-makeGrid(16);
+blackButton.addEventListener('click', () => {
+    blackMode = true;
+    rainbowMode = false;
+    eraserMode = false;
+});
+
+rainbowButton.addEventListener('click', () => {
+    blackMode = false;
+    rainbowMode = true;
+    eraserMode = false;
+});
+
+eraserButton.addEventListener('click', () => {
+    blackMode = false;
+    rainbowMode = false;
+    eraserMode = true;
+});
+
+makeGrid(50);
